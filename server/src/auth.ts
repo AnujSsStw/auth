b5660e57ef15c71e5d32b070eb7d6d4f11dcfc21
anuj <anujzzztw@gmail.com>
2024-11-08 09:23:43 +0530

    add drizzle

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -1,11 +1,23 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { account, session, user, verification } from "./db/schema";
+import { db } from "./db/drizzle";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  // database: prismaAdapter(prisma, {
+  //   provider: "postgresql", // or "mysql", "postgresql", ...etc
+  // }),
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "mysql", "sqlite",
+    schema: {
+      account,
+      session,
+      user,
+      verification,
+    },
   }),
   emailAndPassword: {
     enabled: true,
@@ -17,5 +29,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
-  trustedOrigins: ["http://localhost:5173"], // Replace with the URL of your client-side application}
+  trustedOrigins: [process.env.TRUSTED_ORIGINS!], // Replace with the URL of your client-side application}
 });
